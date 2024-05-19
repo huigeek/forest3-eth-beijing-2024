e38bebd1b5fe57ad97b7e4a1d721b0724bb77fa9
Kirk Lin <linkirk@163.com>
2024-05-19 13:48:38 +0800

    fix: home logo

--- web/src/app/home/page.tsx
+++ web/src/app/home/page.tsx
@@ -28,22 +28,22 @@ function Home() {
     <Flex minHeight="100vh">
       <Box className="relative hidden h-full flex-col bg-[#d4f2c3] p-10 text-white dark:border-r lg:flex" flex="1" height="100vh">
         <div className="absolute inset-0 bg-green-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
-        </div>
-        <img src="/banner.png" c/>
+        {/*<div className="relative z-20 flex items-center text-lg font-medium">*/}
+        {/*  <svg*/}
+        {/*    xmlns="http://www.w3.org/2000/svg"*/}
+        {/*    viewBox="0 0 24 24"*/}
+        {/*    fill="none"*/}
+        {/*    stroke="currentColor"*/}
+        {/*    strokeWidth="2"*/}
+        {/*    strokeLinecap="round"*/}
+        {/*    strokeLinejoin="round"*/}
+        {/*    className="mr-2 h-6 w-6"*/}
+        {/*  >*/}
+        {/*    <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />*/}
+        {/*  </svg>*/}
+        {/*  Logo*/}
+        {/*</div>*/}
+        <img src="/banner.png" />
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
