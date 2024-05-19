8672fd10724a68dc4f4ad3603e9393af0fafdda0
Kirk Lin <linkirk@163.com>
2024-05-19 13:54:46 +0800

    fix: style

--- web/src/components/layout/header.tsx
+++ web/src/components/layout/header.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "~/components/layout/ThemeToggle/theme-toggle";
 export default function Header() {
   return (
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
-      <nav className="h-14 flex items-center justify-between px-4">
+      <nav className="h-16 flex items-center justify-between px-4">
         <div className="hidden lg:block">
           <Link
             href="https://github.com/kirklin/forest3"
